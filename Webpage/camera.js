6d02783ade35c7013ebade0a2ad45432f86edcd9
Alcides Fonseca <me@alcidesfonseca.com>
2008-08-29 15:48:42 +0100

    Webcam capture is now optional

--- Webpage/camera.js
+++ Webpage/camera.js
@@ -1,32 +1,17 @@
-REPEAT_LINK = "Again?";
-
 $id = function(id) {
 	return document.getElementById(id);
 };
 
 handleShot = function(str) {
 	$id('webcamField').value = str;
-	preview = document.createElement("img");
-	preview.src="data:image/png;base64," + str;
-	preview.width=320;
-	preview.height=240;
-	$id('webcam_cont').innerHTML ="";// + str;
-	$id('webcam_cont').appendChild(preview);
-	$id('webcam_cont').innerHTML += "<a href='#' onclick='resetShot(this.parentNode);'>"+REPEAT_LINK+"</a>";
-	
 };
 
-resetShot = function(container) {
-	container.innerHTML = "<object type='application/x-shockwave-flash' data='camera.swf' width='320' height='240'><param name='quality' value='high'/></object>";
-	
-};
 
-window.onload = function() {
-
-	webcam = $id('webcam');
+switch_to_webcam = function(id) {
+	webcam = $id(id);
 	cont = document.createElement("div");
 	cont.id="webcam_cont";
-	resetShot(cont);
+	cont.innerHTML = "<object type='application/x-shockwave-flash' data='camera.swf' width='320' height='240'><param name='quality' value='high'/></object>";
 	webcam.parentNode.replaceChild(cont,webcam);	
 	cont.parentNode.innerHTML += "<input type='hidden' id='webcamField' name='"+webcam.name+"' value=''/>";
 };
