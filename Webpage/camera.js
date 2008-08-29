ad26e0648055d57001491cd7cdaca6f7f34b6e7d
Alcides Fonseca <me@alcidesfonseca.com>
2008-08-29 15:58:23 +0100

    Toggle Webcam now. Optionally Switch and reset.

--- Webpage/camera.js
+++ Webpage/camera.js
@@ -1,17 +1,32 @@
+var webcam = null;
+
 $id = function(id) {
 	return document.getElementById(id);
 };
 
 handleShot = function(str) {
-	$id('webcamField').value = str;
+	$id('webcaminput_field').value = str;
 };
-
 
 switch_to_webcam = function(id) {
 	webcam = $id(id);
 	cont = document.createElement("div");
-	cont.id="webcam_cont";
 	cont.innerHTML = "<object type='application/x-shockwave-flash' data='camera.swf' width='320' height='240'><param name='quality' value='high'/></object>";
 	webcam.parentNode.replaceChild(cont,webcam);	
-	cont.parentNode.innerHTML += "<input type='hidden' id='webcamField' name='"+webcam.name+"' value=''/>";
+	cont.innerHTML += "<input type='hidden' id='webcaminput_field' name='"+webcam.name+"' value=''/>";
+};
+
+reset_to_file = function() {
+	cont = $id('webcaminput_field').parentNode;
+	cont.innerHTML = "";
+	cont.appendChild(webcam);
+	
+};
+
+toggle_webcam = function(id) {
+	if ( $id('webcaminput_field') != null) {
+		reset_to_file();
+	} else {
+		switch_to_webcam(id);
+	}
 };
